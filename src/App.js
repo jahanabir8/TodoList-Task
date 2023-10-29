cb017662b1db54e6fb6e41a8a66166906cdc4552
Abir Islam Anupom <abir@eagle3dstreaming.com>
2023-10-29 15:57:39 +0600

    basic structure for creating todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Create from './Component/CreateTodo/Create';
 
 function App() {
   return (
     <div className="App">
-      
+      <Create></Create>
     </div>
   );
 }
--- src/Component/CreateTodo/Create.js
+++ src/Component/CreateTodo/Create.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Create = () => {
     return (
-        <div>
-            
+        <div className='todo__create__box'>
+            <div className='container'>
+                <div className='create__inner'>
+                    <form className='todo__form'>
+                        <div>   
+                            <label htmlFor='title'>Todo Title</label>
+                            <input type='text' id='title' value={''}/>
+                        </div>
+                        <div>   
+                            <label htmlFor='task'>Todo Title</label>
+                            <input type='text' id='task' value={''}/>
+                        </div>
+                        <div>
+                            <button type='submit'>Add Todo</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
